3683cde164a777380ad9cec4e462ff10a97c861e
Smetan-dot <smetanaandrei@gmail.com>
2023-04-13 11:28:42 +0300

    solutions for queue, linked list and stack

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError, ListNode } = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,9 +22,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let list = new ListNode(-1);
+  list.next = l;
+  let prev = list;
+  let current = l;
+
+  while(current) {
+    if(current.value === k) {
+      prev.next = current.next;
+      current = current.next;
+    }
+    else {
+      prev = current;
+      current = current.next;
+    }
+  }
+
+  return list.next;
 }
 
 module.exports = {
